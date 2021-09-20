1a368f95562ac0fe3e6ee2f1760babd88c7cbef1
mmsesay <maej_01@hotmail.com>
2021-09-20 20:52:16 +0000

    Fix: linter errors

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = 'Hello webpack';
 
   return element;
 }
